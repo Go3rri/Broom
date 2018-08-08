745ef1e8cf2b1d17f3eedd7792ecd5906695f083
Ben Sparkes <bensparkes8@gmail.com>
2018-08-08 16:40:16 +0100

    Init storyboard

---
+++ Broom/Broom/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Broom
+//
+//  Created by Ben Sparkes on 08/08/2018.
+//  Copyright © 2018 Ben Sparkes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
